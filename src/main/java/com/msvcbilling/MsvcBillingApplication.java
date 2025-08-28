1fe5eb64f709565b1b2489c15f6a2f5e038deb75
Raydberg Gabriel <raydbergg@gmail.com>
2025-08-28 12:19:13 -0500

    configuration

--- src/main/java/com/msvcbilling/MsvcBillingApplication.java
+++ src/main/java/com/msvcbilling/MsvcBillingApplication.java
@@ -2,8 +2,9 @@ package com.msvcbilling;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class MsvcBillingApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/msvcbilling/controllers/HelloController.java
@@ -0,0 +1,12 @@
+package com.msvcbilling.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping
+    public String saludo() {
+        return "Hola Microservicio Billing";
+    }
+}
