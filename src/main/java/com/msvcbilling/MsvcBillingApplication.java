988cb40ecf7775d1ba778e81d7d7e7972627f83b
Raydberg Gabriel <raydbergg@gmail.com>
2025-08-28 19:48:55 -0500

    configuration

--- src/main/java/com/msvcbilling/MsvcBillingApplication.java
+++ src/main/java/com/msvcbilling/MsvcBillingApplication.java
@@ -1,9 +1,10 @@
 package com.msvcbilling;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class MsvcBillingApplication {
 
     public static void main(String[] args) {
