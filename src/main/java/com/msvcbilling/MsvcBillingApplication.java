e8475fc5362dd7fe1fb88b7cf38b14c4697d6473
Raydberg Gabriel <raydbergg@gmail.com>
2025-08-29 19:04:08 -0500

    refactor: remove DataSourceAutoConfiguration exclusion from MsvcBillingApplication

--- src/main/java/com/msvcbilling/MsvcBillingApplication.java
+++ src/main/java/com/msvcbilling/MsvcBillingApplication.java
@@ -1,10 +1,9 @@
 package com.msvcbilling;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication
 public class MsvcBillingApplication {
 
     public static void main(String[] args) {
